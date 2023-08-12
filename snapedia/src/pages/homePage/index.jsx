b3180e4fbcae410918cbba43b28a30c59aa1ebc4
JESSICA LEE <jessicalee@macbook-pro.mynetworksettings.com>
2023-08-12 13:15:53 -0700

    add: navbar component

---
+++ snapedia/src/pages/homePage/index.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/system";
+import Navbar from "pages/navbar";
+
+const HomePage = () => {
+    return (
+        <Box>
+            <Navbar />
+        </Box>
+    );
+};
+
+export default HomePage;
